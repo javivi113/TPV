af7a5c1629537d155ac53386434ccfc8486c792a
javia <javiasterio@gmail.com>
2022-12-08 11:28:05 +0100

    gestion articulos

---
+++ ManageArticulos.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace TPV
+{
+    partial class ManageArticulos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            this.filtrobtn = new System.Windows.Forms.Button();
+            this.addbtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtArticulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dblprecio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.intCantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dblImpuesto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtTipo = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.txtArticulo,
+            this.dblprecio,
+            this.intCantidad,
+            this.dblImpuesto,
+            this.txtTipo});
+            this.dgvArticulos.Location = new System.Drawing.Point(12, 111);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.RowTemplate.Height = 25;
+            this.dgvArticulos.Size = new System.Drawing.Size(1158, 499);
+            this.dgvArticulos.TabIndex = 0;
+            // 
+            // filtrobtn
+            // 
+            this.filtrobtn.Location = new System.Drawing.Point(1072, 61);
+            this.filtrobtn.Name = "filtrobtn";
+            this.filtrobtn.Size = new System.Drawing.Size(98, 33);
+            this.filtrobtn.TabIndex = 1;
+            this.filtrobtn.Text = "Buscar";
+            this.filtrobtn.UseVisualStyleBackColor = true;
+            this.filtrobtn.Click += new System.EventHandler(this.hacerFiltro);
+            // 
+            // addbtn
+            // 
+            this.addbtn.Location = new System.Drawing.Point(12, 12);
+            this.addbtn.Name = "addbtn";
+            this.addbtn.Size = new System.Drawing.Size(105, 93);
+            this.addbtn.TabIndex = 2;
+            this.addbtn.Text = "Añadir";
+            this.addbtn.UseVisualStyleBackColor = true;
+            this.addbtn.Click += new System.EventHandler(this.createNewArticulo);
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Location = new System.Drawing.Point(123, 12);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(105, 93);
+            this.deletebtn.TabIndex = 3;
+            this.deletebtn.Text = "Eliminar";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            this.deletebtn.Click += new System.EventHandler(this.deleteSelectedArt);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(907, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 33);
+            this.textBox1.TabIndex = 4;
+            // 
+            // txtArticulo
+            // 
+            this.txtArticulo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.txtArticulo.HeaderText = "Articulo";
+            this.txtArticulo.Name = "txtArticulo";
+            this.txtArticulo.ReadOnly = true;
+            // 
+            // dblprecio
+            // 
+            this.dblprecio.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dblprecio.HeaderText = "Precio";
+            this.dblprecio.Name = "dblprecio";
+            // 
+            // intCantidad
+            // 
+            this.intCantidad.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.intCantidad.HeaderText = "Cantidad";
+            this.intCantidad.Name = "intCantidad";
+            // 
+            // dblImpuesto
+            // 
+            this.dblImpuesto.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dblImpuesto.HeaderText = "Impuesto";
+            this.dblImpuesto.Name = "dblImpuesto";
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.txtTipo.HeaderText = "Tipo";
+            this.txtTipo.Items.AddRange(new object[] {
+            "Cerverza",
+            "Picoteo",
+            "Vino",
+            "Cafes",
+            "Licores",
+            "Varios"});
+            this.txtTipo.Name = "txtTipo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(907, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Buscar un articulo";
+            // 
+            // ManageArticulos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 622);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.addbtn);
+            this.Controls.Add(this.filtrobtn);
+            this.Controls.Add(this.dgvArticulos);
+            this.Name = "ManageArticulos";
+            this.Text = "ManageArticulos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dgvArticulos;
+        private Button filtrobtn;
+        private Button addbtn;
+        private Button deletebtn;
+        private TextBox textBox1;
+        private DataGridViewTextBoxColumn txtArticulo;
+        private DataGridViewTextBoxColumn dblprecio;
+        private DataGridViewTextBoxColumn intCantidad;
+        private DataGridViewTextBoxColumn dblImpuesto;
+        private DataGridViewComboBoxColumn txtTipo;
+        private Label label1;
+    }
+}
---
+++ ManageArticulos.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.VisualBasic;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TPV.Properties;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace TPV
+{
+    public partial class ManageArticulos : Form
+    {
+        public MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
+        {
+            Server = "127.0.0.1",
+            Port = 3333,
+            UserID = "root",
+            Password = "a",
+            Database = "tpv",
+        };
+        public ManageArticulos()
+        {
+            InitializeComponent();
+            connectToDB();
+        }
+        public void connectToDB()
+        {
+            using var connection = new MySqlConnection(builder.ConnectionString);
+            connection.Open();
+            MySqlDataAdapter sqlDA = new MySqlDataAdapter("Select * from articulos", connection);
+            DataTable dtbl = new DataTable();
+            sqlDA.Fill(dtbl);
+            txtArticulo.DataPropertyName = "articulo";
+            dblprecio.DataPropertyName = "precio";
+            intCantidad.DataPropertyName = "cantidad";
+            dblImpuesto.DataPropertyName = "impuesto";
+            txtTipo.DataPropertyName = "tipo";
+           
+            dgvArticulos.DataSource = dtbl;
+        }
+        public void filtrar(String filtro)
+        {
+            if (filtro.Length!=0)
+            {
+                using var connection = new MySqlConnection(builder.ConnectionString);
+                connection.Open();
+
+                MySqlDataAdapter sqlDA = new MySqlDataAdapter("Select * from articulos where articulo=" + filtro, connection);
+                DataTable dtbl = new DataTable();
+                sqlDA.Fill(dtbl);
+                txtArticulo.DataPropertyName = "articulo";
+                dblprecio.DataPropertyName = "precio";
+                intCantidad.DataPropertyName = "cantidad";
+                dblImpuesto.DataPropertyName = "impuesto";
+                txtTipo.DataPropertyName = "tipo";
+
+                dgvArticulos.DataSource = dtbl;
+            }
+            
+        }
+        public bool articsExist(String art)
+        {
+            using var connection = new MySqlConnection(builder.ConnectionString);
+            connection.Open();
+            using var command = connection.CreateCommand();
+            command.CommandText = $"SELECT * FROM articulos WHERE articulo=?art";
+            command.Parameters.Add("?art", MySqlDbType.VarString).Value = art.Trim();
+            Articulos articulos = null;
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    articulos = new Articulos(reader["articulos"].ToString(), reader.GetDouble("precio"), reader.GetInt32("cantidad"), reader.GetDouble("impuesto"), reader["tipo"].ToString());
+                    if (articulos.getArticulo() == art.Trim())
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public bool matchOptionType(String tipo)
+        {
+            switch (tipo)
+            {
+                case "cafes":
+                    return false;
+                case "cerveza":
+                    return false;
+                case "licores":
+                    return false;
+                case "picoteo":
+                    return false;
+                case "vino":
+                    return false;
+                case "refrescos":
+                    return false;
+                case "varios":
+                    return false;
+                default:
+                    return true;
+
+            }
+        }
+        private void createNewArticulo(object sender, EventArgs e)
+        {
+            String articulo;
+            do
+            {
+                articulo = Interaction.InputBox("Introduce un nuevo nombre de articulo: ", "Crear nuevo articulo");
+            } while (articsExist(articulo));
+            double precio=0;
+            bool seguir = true;
+            while (seguir)
+            {
+                try
+                {
+                    precio = Double.Parse(Interaction.InputBox("Introduce el precio para el articulo: ", "Crear nuevo articulo"));
+                    seguir = false;
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            int cantidad=0;
+            seguir = true;
+            while (seguir)
+            {
+                try
+                {
+                    cantidad = Convert.ToInt32(Interaction.InputBox("Introduce el impuesto para el articulo: ", "Crear nuevo articulo"));
+                    if (cantidad > 0)
+                    {
+                        seguir = false;
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            double impuesto=0;
+            seguir = true;
+            while (seguir)
+            {
+                try
+                {
+                    impuesto = Double.Parse(Interaction.InputBox("Introduce el impuesto para el articulo: ", "Crear nuevo articulo"));
+                    seguir = false;
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            String tipo;
+            do
+            {
+                tipo = Interaction.InputBox("Introduce el tipo de articulo:\n-Cerveza\n-Licores\n-Picoteo\n-Vino\n-Varios\n-Cafes ", "Crear nuevo articulo");
+            } while (matchOptionType(tipo));
+            try
+            {
+                using var connection = new MySqlConnection(builder.ConnectionString);
+                connection.Open();
+                using var command = connection.CreateCommand();
+                command.CommandText = $"INSERT INTO articulos VALUES (?articulo,?precio,?cant,?impu,?tipo)";
+                command.Parameters.Add("?articulo", MySqlDbType.VarString).Value = articulo.Trim();
+                command.Parameters.Add("?precio", MySqlDbType.Double).Value = precio;
+                command.Parameters.Add("?cant", MySqlDbType.Int32).Value = cantidad;
+                command.Parameters.Add("?impu", MySqlDbType.Double).Value = impuesto;
+                command.Parameters.Add("?tipo", MySqlDbType.VarString).Value = tipo;
+                command.ExecuteNonQuery();
+                connectToDB();
+            }
+            catch
+            {
+                string msg = "Ha ocurrido un error. No se a podido crear el nuevo articulo.";
+                string cap = "Crear nuevo articulo";
+                MessageBoxButtons btn = MessageBoxButtons.CancelTryContinue;
+                DialogResult res;
+                res = MessageBox.Show(msg, cap, btn);
+            }
+        }
+        private void deleteSelectedArt(object sender, EventArgs e)
+        {
+            if (dgvArticulos.CurrentRow != null)
+            {
+                DataGridViewRow dgvRow = dgvArticulos.CurrentRow;
+                using var connection = new MySqlConnection(builder.ConnectionString);
+                connection.Open();
+                using var command = connection.CreateCommand();
+                command.CommandText = $"DELETE FROM articulos WHERE ariculo=?art ";
+                command.Parameters.Add("?art", MySqlDbType.VarString).Value = dgvRow.Cells["txtarticulo"].Value.ToString();
+                command.ExecuteNonQuery();
+                dgvArticulos.Rows.RemoveAt(dgvRow.Index);
+            }
+        }
+        private void hacerFiltro(object sender, EventArgs e)
+        {
+            filtrar(textBox1.Text);
+        }
+    }
+}
